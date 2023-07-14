12abb0203d9e57763f0cc4526f98244d0b0e17b2
lasto-ed12 <70526829+eo33@users.noreply.github.com>
2023-07-14 14:34:25 +0700

    add like and comment feature

--- README.md
+++ README.md
@@ -1,8 +1,25 @@
 # interactive-image-gallery
 An image gallery web application
 
-# Tools used
+## Tools used
 1. HTML + CSS
 2. Bootstrap
 3. Jquery
 4. Fontawesome
+
+## Features:
+1. Post interaction
+2. Bookmark page view
+
+
+## User stories:
+
+### Post interaction
+1. As a user, I want to be able to view individual post, with likes, comments and bookmark status. (Done!)
+2. As a user, I want to be able to add a comment on a post.
+3. As a user, I want to be able to like a post. (Done!)
+4. As a user, I want to be able to bookmark a post.
+
+### Bookmark page view
+1. As a user, I want to be able to view the list of posts that are bookmark.
+2. As a user, I want to be able to interact with bookmark posts.
--- cardGenerator.js
+++ cardGenerator.js
@@ -9,8 +9,8 @@ function createCard(imagePath,title) {
     // icons
     let cardOverlay = $('<div class="card-img-overlay d-inline-flex justify-content-center align-items-center">')
     let hider = $('<div class="hide">').attr('id',title+'-hide')
-    let like = $('<i class="fa-solid fa-thumbs-up p-3 hide"  style="color: #ffffff" > 1</i>').attr('id',title+'-like');
-    let comment = $('<i class="fa-solid fa-comment p-3 hide" style="color: #ffffff;" > 1</i>');
+    let like = $('<i class="fa-solid fa-thumbs-up p-3 hide"  style="color: #ffffff" > </i>').attr('id',title+'-like');
+    let comment = $('<i class="fa-solid fa-comment p-3 hide" style="color: #ffffff;" > </i>');
 
     // Append the icons to the image overlay
     cardOverlay.append(hider)
@@ -26,8 +26,11 @@ function createCard(imagePath,title) {
     $('#cardContainer').append(cardCol);
 }
 
+
 $(document).ready(function() {
     let imageTracker = 1;
+    let counter = [0,0,0,0,0,0]
+
     for (let i = 1; i <= 6; i++){
         createCard(`./public/${i}.jpg`,'card-num-'+i);
 
@@ -48,28 +51,43 @@ $(document).ready(function() {
         $('#card-num-'+i).on('click',()=>{
             $('#fullscreen').removeClass('hide')
             $('#selected-image').attr('src',`./public/${i}.jpg`)
+            imageTracker = i;
             if(i == 1){
                 $('#left-arrow').addClass('hide')
              } else{
                 $('#left-arrow').removeClass('hide')
              }
+            $('.fa-thumbs-up').on('click', () => {
+                $('#card-num-'+i+'-like').addClass('fa-solid');
+            });
          })
-
     }
     
     $('#right-arrow').on('click',()=>{
         imageTracker += 1;
         $('#selected-image').attr('src',`./public/${imageTracker}.jpg`)
+        $('#left-arrow').removeClass('hide'); // Always show the left arrow
+        if (imageTracker === 6) {
+            $('#right-arrow').addClass('hide'); // Hide the right arrow when at image 6
+        }
     })
 
     $('#left-arrow').on('click',()=>{
         imageTracker -= 1;
         $('#selected-image').attr('src',`./public/${imageTracker}.jpg`)
+        $('#right-arrow').removeClass('hide'); // Always show the right arrow
+        if (imageTracker === 1) {
+            $('#left-arrow').addClass('hide'); // Hide the left arrow when at image 1
+        }
     })
     $("#exit-carousel").on('click',()=>{
         $('#fullscreen').addClass('hide')
         imageTracker=0;
      })
 
+
+    $('.fa-comment').on('click',(event)=>{
+        
+    })
 });
   
